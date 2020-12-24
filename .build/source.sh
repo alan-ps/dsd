fa9b2433541e1744b0aae6084d43df8865b80e3f
Serhii Puchkovskyi <serhii_puchkovskyi@Serhiis-MacBook-Pro.local>
2020-12-25 00:35:41 +0200

    Initial commit.

---
+++ .build/source.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Exec drush command from the php container.
+drush="../vendor/bin/drush -r /var/www/web"
+
+# The .sql file inside a container (use volume in order to import from a host).
+sqlfile='/var/www/web/file.sql'
+
+# Helper function to install a Drupal 8 site from scratch.
+# @todo use a variable to set a profile!
+drupal_install_from_cratch() {
+  docker-compose exec --user root php /bin/sh -ci "$drush si standard --yes \
+    --account-name=admin \
+    --account-pass=admin \
+    --site-name='Drupal 9 standard' \
+    --db-url=mysql://drupal:drupal@mariadb/drupal
+  "
+}
+
+# Helper function to install a Drupal 8 site from existing database.
+drupal_install_from_existing_db() {
+  docker-compose exec --user root php /bin/sh -ci "$drush sql-drop --yes"
+  docker-compose exec --user root php /bin/sh -ci "$drush sqlc < $sqlfile"
+}
+
+# Helper function to install a Drupal 8 site using existing configuration.
+# @see https://www.drupal.org/project/config_installer
+drupal_install_from_config_installer() {
+  echo "@todo";
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# D8D (Drupal 9 Docker)
+Easy way to deploy Drupal 9 project (fresh installation or from with existing database) using Docker.
